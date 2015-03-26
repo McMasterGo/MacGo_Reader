ff6dcaf535e1092e60673c8d3c1af111a91e0485
Simon Quach <quachsh@mcmaster.ca>
2015-03-26 15:45:24 -0400

    first commit

---
+++ QRCodeReader/ItemController.h
@@ -0,0 +1,13 @@
+//
+//  ItemController.h
+//  QRCodeReader
+//
+//  Created by Simon Quach on 2015-02-08.
+//  Copyright (c) 2015 Appcoda. All rights reserved.
+//
+
+#import <ParseUI/ParseUI.h>
+
+@interface ItemController : PFQueryTableViewController
+
+@end
